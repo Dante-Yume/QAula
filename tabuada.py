4ea1259ca34eda717c6118b96e5cb4b3b07c1091
Dante-Yume <Danteebp@gmail.com>
2025-10-30 19:44:50 -0300

    venv

--- tabuada.py
+++ tabuada.py
@@ -1,7 +1,4 @@
-numero = int(input('digite um numero para ver a tabuada: '))
-
-print(f'\nTabuada do {numero}:\n')
-for i in range (1, 11):
-    resultado = numero * i
-    print(f'{numero} x {i} = {resultado}')
-    print("\nTabuada concluida!")
+for multi in range (1,11):
+    for numero in range (1,11):
+        calculo= multi * numero
+        print(f'{multi} X {numero} = {calculo}')
