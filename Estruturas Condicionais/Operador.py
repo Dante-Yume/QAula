717248e475587979efce25d5e5bfacdc76205707
Dante-Yume <Danteebp@gmail.com>
2025-11-04 20:00:06 -0300

    atualizacao final

---
+++ Estruturas Condicionais/Operador.py
@@ -0,0 +1,16 @@
+valor = float(input("Digite um valor numérico: "))
+
+
+dobro = valor * 2
+triplo = valor * 3
+quadrado = valor ** 2
+raiz_quadrada = valor ** 0.5
+raiz_cubica = valor ** (1/3)
+
+
+print("\nResultados:")
+print("1️ Dobro do valor:", dobro)
+print("2️ Triplo do valor:", triplo)
+print("3️ Valor ao quadrado:", quadrado)
+print("4️ Raiz quadrada:", round(raiz_quadrada, 2))
+print("5️ Raiz cúbica:", round(raiz_cubica, 2))
